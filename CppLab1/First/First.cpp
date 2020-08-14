35b797e198fc709465ffdde543d9418e334bcdb8
Ntail <nikita31122001@gmail.com>
2020-08-14 10:44:44 +0300

    ready

--- CppLab1/First/First.cpp
+++ CppLab1/First/First.cpp
@@ -6,10 +6,20 @@
 #include <cstdlib>
 #include <ctime>
 
+void print(int arr[], int n)
+{
+	printf("\nArray:\n");
+	for (int i = 0; i < n; i++)
+	{
+		printf("| %d ", arr[i]);
+	}
+}
+
 void processArray(int arr[], int n, int a, int b)
 {
 	srand((unsigned int)time(NULL));
-	int i = 0, cnt = 0;
+	int i = 0;
+	size_t cnt = 0;
 	while (i < n)
 	{
 		arr[i] = arr[i] * ((rand() % (b-a+1)) + a);
@@ -17,14 +27,28 @@ void processArray(int arr[], int n, int a, int b)
 			cnt++; //считаем кол-во отрицательных (как раз будет размером массива)
 		i+=2;
 	}
+	print(arr, n);
 
-	int newArr[cnt]; //чтобы создать массив, нужно указать размер в виде константы. надо придумать, как int cnt перевести в const int
+	int* newArr = new int[cnt];
 
-	printf("\nArray:\n");
+	for (int i = 0; i < cnt; i++)
+		newArr[i] = 0;
+
 	for (int i = 0; i < n; i++)
 	{
-		printf("%d\n", arr[i]);
+		if (arr[i] < 0)
+		{
+			for (int j = 0; j < cnt; j++)
+			{
+				if (newArr[j] == 0)
+				{
+					newArr[j] = arr[i];
+					break;
+				}
+			}
+		}
 	}
+	print(newArr, cnt);
 }
 
 int main()
