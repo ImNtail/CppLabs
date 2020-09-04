83536d576d568192e2d03145cebdc5b3f9babd8c
Ntail <nikita31122001@gmail.com>
2020-09-04 19:43:36 +0300

    Comment with task

--- CppLab1/First/First.cpp
+++ CppLab1/First/First.cpp
@@ -1,5 +1,12 @@
 // First.cpp : Defines the entry point for the console application.
 //
+/*ќбъ€вить массив из n=15 вещественных чисел, проинициализировать нул€ми. ‘ункци€
+processArray() должна заполнить массив случайными числами от 20.0 до 100.0, а
+затем вычесть из каждого элемента массива минимальное значение в массиве. Ёто
+значение она также должна вернуть на выходе. —формировать выходной
+вещественный массив, в котором все элементы, сто€щие до позиции минимального
+элемента включительно, повтор€ют элементы входного массива, а остальные равны
+минимальному элементу массива. ¬ывести на экран результирующие массивы.*/
 
 #define _CRT_SECURE_NO_WARNINGS
 #include "stdafx.h"
@@ -15,14 +22,14 @@ void print(float arr[], int n)
 	}
 }
 
-void processArray(float arr[], int n, float a, int b)
+void processArray(float arr[], int n, float a, float b)
 {
 	srand((unsigned int)time(NULL));
 	float min = 1000;
 	int pos = 0;
 	for (int i = 0; i < n; i++)
 	{
-		arr[i] = (float)(rand() % b - a) + a;
+		arr[i] = ((b - a) * ((float)rand() / RAND_MAX)) + a;
 		if (arr[i] < min)
 		{
 			min = arr[i];
@@ -44,6 +51,7 @@ void processArray(float arr[], int n, float a, int b)
 			newArr[i] = min;
 	}
 	print(newArr, n);
+	printf("Position of ");
 }
 
 int main()
