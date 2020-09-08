20fed8ec1119e61eaf458d62825544611e027cbe
Ntail <nikita31122001@gmail.com>
2020-09-08 11:41:38 +0300

    correction of the first

--- CppLab1/Second/Second.cpp
+++ CppLab1/Second/Second.cpp
@@ -9,14 +9,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "iostream";
 
+void arrInit(int (&arr)[5][5])
+{
+
+}
+
 void main()
 {
 	const int n = 5;
-	int* arr = new int[n,n];
+	int arr[n][n];
 	arrInit(arr);
-}
-
-void arrInit(int arr)
-{
-
 }
