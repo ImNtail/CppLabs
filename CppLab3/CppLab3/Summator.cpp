6d0e29b250230fae48ab87fef08c9e79a5effb2e
Ntail <nikita31122001@gmail.com>
2020-11-25 20:01:05 +0300

    doing...

--- CppLab3/CppLab3/Summator.cpp
+++ CppLab3/CppLab3/Summator.cpp
@@ -8,22 +8,27 @@ Summator::Summator(int n) : ExpressionEvaluator(n) { }
 
 double Summator::calculate()
 {
-    double output;
-    for (int i = 0; i < n_; i++)
-    {
-        i == 0 ? (output = ops_[i]) : (output -= ops_[i]);
-    }
+    double output = ops_[0];
+    for (int i = 1; i < n_; i++)
+        output += ops_[i];
     return output;
 }
 
 void Summator::logToScreen()
 {
     for (int i = 0; i < n_; i++)
+        std::cout << "Op" << i + 1 << ", ";
+    std::cout << ": ";
+    for (int i = 0; i < n_; i++)
     {
-        (ops_[i] >= 0 ?
-            std::cout << ops_[i] :
-            std::cout << '(' << ops_[i] << ')') //add parenthesis when operand is negative
-            << (i != n_ - 1 ? (" minus ") : " equals ");
+        if (ops_[i] >= 0)
+            std::cout << ops_[i] << " ";
+        else
+            std::cout << "(" << ops_[i] << ") ";
+        if (i == n_ - 1)
+            std::cout << "\n-> ";
+        else
+            std::cout << "+ ";
     }
     std::cout << calculate() << std::endl;
 }
@@ -33,11 +38,18 @@ void Summator::logToFile(const std::string& filename)
     std::ofstream file;
     file.open(filename);
     for (int i = 0; i < n_; i++)
+        file << "Op" << i + 1 << ", ";
+    file << ": ";
+    for (int i = 0; i < n_; i++)
     {
-        (ops_[i] >= 0 ?
-            std::cout << ops_[i] :
-            std::cout << '(' << ops_[i] << ')') //add parenthesis when operand is negative
-            << (i != n_ - 1 ? (" minus ") : " equals ");
+        if (ops_[i] >= 0)
+            file << ops_[i] << " ";
+        else
+            std::cout << "(" << ops_[i] << ") ";
+        if (i == n_ - 1)
+            file << "\n-> ";
+        else
+            file << "+ ";
     }
     file << calculate() << std::endl;
 }
